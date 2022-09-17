28be93901170d89c3cbd9970f06e3de2611aa267
Miqueias Sousa <mi.queias2010@hotmail.com>
2022-09-17 01:13:01 -0300

    Integration test for update method

--- products-service/src/test/kotlin/br/com/products/resources/ProductResourcesTestIT.kt
+++ products-service/src/test/kotlin/br/com/products/resources/ProductResourcesTestIT.kt
@@ -2,21 +2,32 @@ package br.com.products.resources
 
 import br.com.products.FindByIdServiceRequest
 import br.com.products.ProductServiceRequest
+import br.com.products.ProductServiceUpdateRequest
 import br.com.products.ProductsServiceGrpc.ProductsServiceBlockingStub
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.flywaydb.core.Flyway
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 @MicronautTest
 internal class ProductResourcesTestIT(
+    private val flyway: Flyway,
     private val productsServiceBlockingStub: ProductsServiceBlockingStub
 ) {
+
+    @BeforeEach
+    fun setUp() {
+        flyway.clean()
+        flyway.migrate()
+    }
+
     @Test
-    fun `when ProductsServiceGrpc create method is call with valid should be return success`() {
+    fun `when ProductsServiceGrpc create method is call with valid data should be return success`() {
         val request = ProductServiceRequest.newBuilder()
             .setName("product name")
             .setPrice(20.10)
@@ -25,7 +36,8 @@ internal class ProductResourcesTestIT(
 
         val response = productsServiceBlockingStub.create(request)
 
-        assertEquals(2, response.id)
+        // for any product add in migrate need to plus id
+        assertEquals(3, response.id)
         assertEquals("product name", response.name)
         assertEquals(20.10, response.price)
     }
@@ -72,6 +84,74 @@ internal class ProductResourcesTestIT(
         }
 
         assertEquals(Status.ALREADY_EXISTS.code, response.status.code)
+        assertEquals(description, response.status.description)
+    }
+
+    @Test
+    fun `when ProductsServiceGrpc update method is call with valid data should be return success`() {
+        val request = ProductServiceUpdateRequest.newBuilder()
+            .setId(1L)
+            .setName("update name")
+            .setPrice(20.10)
+            .setQuantityInStock(10)
+            .build()
+
+        val response = productsServiceBlockingStub.update(request)
+
+        assertEquals(1, response.id)
+        assertEquals("update name", response.name)
+        assertEquals(20.10, response.price)
+    }
+
+    @Test
+    fun `when ProductsServiceGrpc update method is call with invalid data should be return success`() {
+        val request = ProductServiceUpdateRequest.newBuilder()
+            .setId(2L)
+            .setName("update name")
+            .setPrice(20.10)
+            .setQuantityInStock(10)
+            .build()
+
+        val response = productsServiceBlockingStub.update(request)
+
+        assertEquals(2, response.id)
+        assertEquals("update name", response.name)
+        assertEquals(20.10, response.price)
+    }
+
+    @Test
+    fun `when ProductsServiceGrpc update method is call with invalid data a AlreadyExistsException is returned`() {
+        val request = ProductServiceUpdateRequest.newBuilder()
+            .setId(1L)
+            .setName("Product A")
+            .setQuantityInStock(10)
+            .build()
+
+        val description = "Produto [ ${request.name} ] já cadastrado no sistema"
+
+        val response = assertThrows<StatusRuntimeException> {
+            productsServiceBlockingStub.update(request)
+        }
+
+        assertEquals(Status.ALREADY_EXISTS.code, response.status.code)
+        assertEquals(description, response.status.description)
+    }
+
+    @Test
+    fun `when ProductsServiceGrpc update method is call with invalid id should be return ProductNotFoundException`() {
+        val request = ProductServiceUpdateRequest.newBuilder()
+            .setId(5L)
+            .setName("Product C")
+            .setQuantityInStock(10)
+            .build()
+
+        val description = "Produto com id [ ${request.id} ] não cadastrado no sistema"
+
+        val response = assertThrows<StatusRuntimeException> {
+            productsServiceBlockingStub.update(request)
+        }
+
+        assertEquals(Status.NOT_FOUND.code, response.status.code)
         assertEquals(description, response.status.description)
     }
 }
